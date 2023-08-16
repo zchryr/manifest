0f3e2a07d5f411b9dd9216452bd64a8658b917f2
Zachary R <76828749+zchryr@users.noreply.github.com>
2023-08-15 22:12:56 -0400

    Init.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/zchryr/manifest
+
+go 1.21.0
+
+require (
+	github.com/sethvargo/go-envconfig v0.9.0 // indirect
+	github.com/sethvargo/go-githubactions v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/sethvargo/go-githubactions"
+)
+
+type ManifestInfo struct {
+	Full string `json:"full"`
+	Change string `json:"change"`
+	Short string `json:"short"`
+}
+
+var manifests []ManifestInfo
+
+func find(manifest string) {
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			return err
+		}
+
+		split := strings.Split(path, "/")
+		if split[len(split)-1] == manifest {
+			m := ManifestInfo {
+				Short: strings.TrimRight(strings.Split(path, manifest)[0], "/"),
+				Change: "./" + strings.Split(path, manifest)[0],
+				Full: "./" + path,
+			}
+
+			manifests = append(manifests, m)
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+	find("package.json")
+
+	j, _ := json.Marshal(manifests)
+	// fmt.Println(string(j))
+
+	githubactions.SetOutput("matrix", string(j))
+}
