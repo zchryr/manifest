e495e1326265b35bf7c3bed3eb9a42ec243d67cc
Zachary R <76828749+zchryr@users.noreply.github.com>
2023-10-17 19:23:24 -0400

    Wildcard functionality.

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
+const glob = require('glob');
 
 // Function to retrieve inputs.
-// This abstracts the difference between running inside GitHub Actions and running locally.
+// This function abstracts the difference between running inside GitHub Actions and running locally.
 function getInput(inputName) {
   // If running within GitHub Actions, use the @actions/core package.
   if (process.env.GITHUB_ACTION) {
@@ -15,8 +16,9 @@ function getInput(inputName) {
   }
 }
 
-// Retrieve the name of the manifest file to search for.
-const manifestFileName = path.basename(getInput('manifest'));
+// Retrieve the pattern for the manifest files to search for.
+// This can include wildcards, e.g., "*.csproj".
+const manifestPattern = getInput('manifest');
 
 // Retrieve the directories to ignore.
 // This splits the input based on commas, and trims any whitespace.
@@ -42,48 +44,30 @@ function isIgnored(directory) {
   });
 }
 
-// Recursive function to search for the manifest file in a directory and its subdirectories.
+// Function to search for files matching the manifest pattern in a directory and its subdirectories.
 function searchForFiles(directory, resultArray) {
-  try {
-    // Read all files and directories within the current directory.
-    const files = fs.readdirSync(directory);
+  // Use the glob package to search for files matching the manifest pattern.
+  // Note: ** matches all directories; * matches within the current directory.
+  glob.sync(`${directory}/**/${manifestPattern}`).forEach((filePath) => {
+    const directoryOfFoundFile = path.dirname(filePath);
 
-    for (const file of files) {
-      // Formulate a full path for the file or directory.
-      const filePath = path.join(directory, file);
-      // Get details like if it's a file or directory.
-      const stat = fs.statSync(filePath);
-
-      if (stat.isDirectory()) {
-        // If it's a directory and not in the ignore list, search within it.
-        if (!isIgnored(filePath)) {
-          searchForFiles(filePath, resultArray);
-        }
-      }
-      // If it's the manifest file and we're either not ignoring the root
-      // or the current directory isn't the root, add its details to the result.
-      else if (file === manifestFileName) {
-        if (!ignoreRoot || directory !== process.cwd()) {
-          resultArray.push({
-            // Only the directory path, not including the manifest file name.
-            relativePath: path.relative(process.cwd(), directory),
-            fullPath: filePath,
-            cd: path.dirname(filePath),
-          });
-        }
-      }
+    // Check if this directory should be ignored
+    // or if it's the root directory and we are ignoring the root.
+    if (
+      !isIgnored(directoryOfFoundFile) &&
+      (!ignoreRoot || directoryOfFoundFile !== process.cwd())
+    ) {
+      resultArray.push({
+        relativePath: path.relative(process.cwd(), directoryOfFoundFile),
+        fullPath: filePath,
+        cd: directoryOfFoundFile,
+      });
     }
-  } catch (error) {
-    // In case of an error (like permission issues), log it.
-    console.error(
-      `Error processing directory: ${directory}. Reason: ${error.message}`
-    );
-  }
+  });
 }
 
 // Start the search from the current working directory.
 const currentDirectory = process.cwd();
-
 // This array will store details of all the found manifest files.
 const foundFiles = [];
 
@@ -94,9 +78,9 @@ searchForFiles(currentDirectory, foundFiles);
 const matrix = JSON.stringify(foundFiles, null, 2);
 
 // If we're in GitHub Actions, set the matrix as an output.
+// Otherwise, print the JSON string for visualization when running locally.
 if (process.env.GITHUB_ACTION) {
   core.setOutput('matrix', matrix);
 } else {
-  // Print the JSON string for visualization (useful both locally and in GitHub Actions logs).
   console.log(matrix);
 }
