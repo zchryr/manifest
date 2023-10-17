f03509d7fcb04a28c30a24aacb91858380607df1
Zachary R <76828749+zchryr@users.noreply.github.com>
2023-10-17 16:16:00 -0400

    testing

--- main.js
+++ main.js
@@ -2,11 +2,32 @@ const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
 
+// Get the input values from the workflow YAML file
 const manifestPath = core.getInput('manifest');
 const ignoreDirs = core.getInput('manifest_ignore')
   ? core.getInput('manifest_ignore').split(',')
   : [];
 
+function isIgnored(directory) {
+  return ignoreDirs.some((ignoreDir) => {
+    const ignoreParts = ignoreDir.split('/').filter((part) => part.length > 0);
+    const directoryParts = directory
+      .split('/')
+      .filter((part) => part.length > 0);
+
+    // Check if the directory path matches the 'manifest_ignore' path
+    if (
+      ignoreParts.length > 0 &&
+      ignoreParts.every((part, index) => part === directoryParts[index])
+    ) {
+      return true; // The directory should be ignored
+    }
+
+    return false; // The directory should not be ignored
+  });
+}
+
+// Function to search for files and directories in the current directory and subdirectories
 function searchForFiles(directory, resultArray) {
   const files = fs.readdirSync(directory);
 
@@ -15,7 +36,8 @@ function searchForFiles(directory, resultArray) {
     const stat = fs.statSync(filePath);
 
     if (stat.isDirectory()) {
-      if (!ignoreDirs.includes(file)) {
+      // Check if the directory should be ignored
+      if (!isIgnored(filePath)) {
         searchForFiles(filePath, resultArray); // Recursively search in subdirectories
       }
     } else if (file === manifestPath) {
@@ -28,13 +50,18 @@ function searchForFiles(directory, resultArray) {
   }
 }
 
+// Get the current working directory
 const currentDirectory = process.cwd();
 const foundFiles = [];
 
+// Start the search for files and directories
 searchForFiles(currentDirectory, foundFiles);
 
 // Convert the foundFiles array to a JSON string
 const matrix = JSON.stringify(foundFiles, null, 2);
 
-core.setOutput('matrix', matrix);
+// Set the 'matrix' output to the JSON string
+// core.setOutput('matrix', matrix);
+
+// Print the JSON string to the console
 console.log(matrix);
