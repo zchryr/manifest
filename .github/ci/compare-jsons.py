6bdf0e6bf2d2fc6d3e5c247ab0b3f4d712f24c63
Zachary R <76828749+zchryr@users.noreply.github.com>
2023-10-24 13:54:08 -0400

    testing.

--- .github/ci/compare-jsons.py
+++ .github/ci/compare-jsons.py
@@ -7,12 +7,11 @@ def compare_jsons(json1, json2):
 
 
 if __name__ == "__main__":
-    # env_json_string = os.environ.get("ENV_JSON_STRING")
+    env_json_string = os.environ.get("ENV_JSON_STRING")
     file_json_string = os.environ.get("FILE_JSON_STRING")
-    print(file_json_string)
 
-    # if not compare_jsons(env_json_string, file_json_string):
-    #     print("JSONs are not the same!")
-    #     exit(1)
-    # else:
-    #     print("JSONs match!")
+    if not compare_jsons(env_json_string, file_json_string):
+        print("JSONs are not the same!")
+        exit(1)
+    else:
+        print("JSONs match!")
