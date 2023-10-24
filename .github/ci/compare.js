ee415d2b41e170dda68eb84994d89619f1e4ba45
Zachary R <76828749+zchryr@users.noreply.github.com>
2023-10-24 14:45:04 -0400

    test

--- .github/ci/compare.js
+++ .github/ci/compare.js
@@ -3,9 +3,15 @@ const fs = require('fs');
 const inputObjectPath = process.argv[2];
 const localJsonPath = process.argv[3];
 
-// Read and parse the input object
-const rawObjectData = fs.readFileSync(inputObjectPath, 'utf-8');
-const translatedObject = eval('(' + rawObjectData + ')');
+// Read and adjust the string to make it valid JSON
+const rawObjectData = fs
+  .readFileSync(inputObjectPath, 'utf-8')
+  .replace(/(\w+):/g, '"$1":') // wrap keys without quotes with quotes
+  .replace(/: ([^"]\S*[^"]),/g, ': "$1",') // wrap values without quotes with quotes
+  .replace(/: ([^"]\S*[^"])\n/g, ': "$1"\n'); // wrap values without quotes that end on a newline with quotes
+
+// Now it should be a valid JSON string, so we parse it
+const translatedObject = JSON.parse(rawObjectData);
 
 // Read and parse the local JSON file
 const rawJsonData = fs.readFileSync(localJsonPath, 'utf-8');
