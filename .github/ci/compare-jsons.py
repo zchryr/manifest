8d48528e75df3d9131893a1e3ca17e909c465148
Zachary R <76828749+zchryr@users.noreply.github.com>
2023-10-24 13:46:35 -0400

    Try python.

---
+++ .github/ci/compare-jsons.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+
+def compare_jsons(json1, json2):
+    return json.loads(json1) == json.loads(json2)
+
+
+if __name__ == "__main__":
+    # env_json_string = os.environ.get("ENV_JSON_STRING")
+    file_json_string = os.environ.get("FILE_JSON_STRING")
+    print(file_json_string)
+
+    # if not compare_jsons(env_json_string, file_json_string):
+    #     print("JSONs are not the same!")
+    #     exit(1)
+    # else:
+    #     print("JSONs match!")
